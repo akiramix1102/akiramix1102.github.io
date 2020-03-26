302fed355bbcf44f642e23477f9d57a132525ccf
Nguyen Dat <akiramix1102@gmail.com>
2020-03-26 17:31:16 +0700

    add page contact.html

--- isurlife/assets/js/main.js
+++ isurlife/assets/js/main.js
@@ -1,4 +1,19 @@
 $(document).ready(function () {
 
-    $("#myform").validate();
+    var btn = $('#scroll-top');
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 500) {
+            btn.addClass('show');
+        } else {
+            btn.removeClass('show');
+        }
+    });
+    btn.on('click', function (e) {
+        e.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: 0
+        }, '300');
+    });
 });
+new WOW().init();
